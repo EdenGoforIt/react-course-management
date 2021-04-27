d3eb80e857b55801bef8d79bf526d4111eda42e8
Eden Park <edenGoforit@gmail.com>
2021-04-27 21:08:24 +1200

    Feat: page not found page with Switch

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
 import Header from "./common/Header";
+import PageNotFound from "./PageNotFound";
 
 function App() {
   return (
     <div className="container-fluid">
-      <Header />
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/about" component={AboutPage} />
+      <Switch>
+        <Header />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={AboutPage} />
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/PageNotFound.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return <h1>Opps! Page Not Found</h1>;
+};
+export default PageNotFound;
