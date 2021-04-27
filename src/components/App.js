46d34007f6082c3e105e4b2eb19cc69e37b3ba4a
Eden Park <edenGoforit@gmail.com>
2021-04-27 21:52:28 +1200

    Feat: course page placeholder added

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
 import Header from "./common/Header";
 import PageNotFound from "./PageNotFound";
+import CoursesPage from "../courses/CoursesPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Header />
         <Route exact path="/" component={HomePage} />
         <Route exact path="/about" component={AboutPage} />
+        <Route exact path="/courses" component={CoursesPage} />
         <Route component={PageNotFound} />
       </Switch>
     </div>
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -13,6 +13,10 @@ function Header() {
       <NavLink to="/about" activeStyle={activeStyle}>
         About
       </NavLink>
+      {" | "}
+      <NavLink to="/courses" activeStyle={activeStyle}>
+        Courses
+      </NavLink>
     </nav>
   );
 }
