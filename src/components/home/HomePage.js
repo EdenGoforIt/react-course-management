64beba3b3dcdefa6967125bd1b9db8efa3444349
Eden Park <edenGoforit@gmail.com>
2021-05-02 15:04:20 +1200

    Feat: Mock API implemented

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => (
   <div className="jumbotron">
-    <h1>Hi</h1>
-    <Link to="about" className="btn btn-primary btn-lg"></Link>
+    <h1>Course Management by Eden Park</h1>
+    <p>Manage your course with Redux</p>
+    <Link to="about" className="btn btn-primary btn-lg">
+      About Us
+    </Link>
   </div>
 );
 export default HomePage;
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,7 @@
 const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { DefinePlugin } = require("webpack");
 
 process.env.NODE_ENV = "development";
 
@@ -12,7 +13,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "build"),
     publicPath: "/",
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
   devServer: {
     stats: "minimal",
@@ -20,25 +21,28 @@ module.exports = {
     historyApiFallback: true,
     disableHostCheck: true,
     headers: { "Access-Control-Allow-Origin": "*" },
-    https: false
+    https: false,
   },
   plugins: [
+    new DefinePlugin({
+      "process.env.API_URL": JSON.stringify("http://localhost:3001"),
+    }),
     new HtmlWebpackPlugin({
-      template: "src/index.html"
+      template: "src/index.html",
       // favicon: "src/favicon.ico",
-    })
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader", "eslint-loader"]
+        use: ["babel-loader", "eslint-loader"],
       },
       {
         test: /(\.css)$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  }
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
