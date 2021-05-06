68ac03ba715ebda10cf6bdd74b9787f204b735ee
Eden Park <edenGoforit@gmail.com>
2021-05-06 21:56:16 +1200

    Feat: update and save actions async functions

--- src/components/courses/ManageCoursesPage.js
+++ src/components/courses/ManageCoursesPage.js
@@ -1,12 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { loadCourses } from "../../redux/actions/courseActions";
+import { loadCourses, saveCourse } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
-import { bindActionCreators } from "redux";
-import CourseList from "./CourseList";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
 
-function ManageCoursesPage({ courses, authors, loadAuthors, loadCourses }) {
+function ManageCoursesPage({
+  courses,
+  authors,
+  loadAuthors,
+  loadCourses,
+  saveCourse,
+  ...props
+}) {
+  const [course, setCourse] = useState(...props.course);
+  const [errors, setErrors] = useState({});
+  const [saving, setSaving] = useState(false);
+
   useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch((err) => console.error(err));
@@ -16,10 +27,51 @@ function ManageCoursesPage({ courses, authors, loadAuthors, loadCourses }) {
     }
   }, []);
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setCourse((prevCourse) => ({
+      ...prevCourse,
+      [name]: name === "authorId" ? parseInt(value, 10) : value,
+    }));
+  }
+
+  function formIsValid() {
+    const { title, authorId, category } = course;
+    const errors = {};
+
+    if (!title) errors.title = "Title is required.";
+    if (!authorId) errors.author = "Author is required";
+    if (!category) errors.category = "Category is required";
+
+    setErrors(errors);
+    // Form is valid if the errors object still has no properties
+    return Object.keys(errors).length === 0;
+  }
+
+  function handleSave(event) {
+    event.preventDefault();
+    if (!formIsValid()) return;
+    setSaving(true);
+    saveCourse(course)
+      .then(() => {
+        // toast.success("Course saved.");
+        history.push("/courses");
+      })
+      .catch((error) => {
+        setSaving(false);
+        setErrors({ onSave: error.message });
+      });
+  }
+
   return (
-    <>
-      <h2>Manage Courses</h2>
-    </>
+    <CourseForm
+      course={course}
+      errors={errors}
+      authors={authors}
+      onChange={handleChange}
+      onSave={handleSave}
+      saving={saving}
+    />
   );
 }
 
@@ -28,11 +80,13 @@ ManageCoursesPage.propTypes = {
   loadAuthors: PropTypes.func.isRequired,
   loadCourses: PropTypes.func.isRequired,
   authors: PropTypes.array.isRequired,
+  course: PropTypes.object.isRequired,
 };
 
 //same as state => return courses: state.courses
 function mapStateToProps(state) {
   return {
+    course: newCourse,
     authors: state.authors,
     courses: state.courses,
   };
--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -7,6 +7,12 @@ export function createCourse(course) {
 export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+export function saveCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
 
 export function loadCourses() {
   return function (dispatch) {
@@ -14,6 +20,22 @@ export function loadCourses() {
       .getCourses()
       .then((courses) => {
         dispatch({ type: types.LOAD_COURSES_SUCCESS, courses });
+        //or  dispatch(loadCoursesSuccess(courses));
+      })
+      .catch((error) => {
+        throw error(error);
+      });
+  };
+}
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    return courseApi
+      .saveCourse(course)
+      .then((savedCourse) => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(saveCourseSuccess(savedCourse));
+
         //or  dispatch(loadCoursesSuccess(courses));
       })
       .catch((error) => {
